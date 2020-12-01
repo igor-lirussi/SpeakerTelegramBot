b014a62a8eb3ad5591354887971d544ab6d02cbe
Blaac <ruben.ceroni@studio.unibo.it>
2020-12-01 13:09:59 +0100

    update install again

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ apt update
 apt install mplayer pulseaudio python3-pip -y
 echo default-server = $1 >> /etc/pulse/client.conf 
 pip install -r requirements.txt
+mv speakerbot.service /etc/systemd/system/
 systemctl enable speakerbot
 systemctl start speakerbot
