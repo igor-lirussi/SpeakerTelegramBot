28fa55123f4de398734f756ad7062079f88cb968
Blaac <ruben.ceroni@studio.unibo.it>
2020-12-01 12:46:46 +0100

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt update
 apt install mplayer pulseaudio -y
-echo default-server = 192.168.1.98 >> /etc/pulse/client.conf 
+echo default-server = $1 >> /etc/pulse/client.conf 
 pip install -r /app/requirements.txt
