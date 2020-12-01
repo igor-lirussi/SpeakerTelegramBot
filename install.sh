c7cca9ac2120f560efd72f9c6a4e61b4df6c7ac8
Blaac <ruben.ceroni@studio.unibo.it>
2020-12-01 14:11:27 +0100

    add missing dep

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-apt install mplayer pulseaudio python3-pip -y
+apt install ffmpeg mplayer pulseaudio python3-pip -y
 echo default-server = $1 >> /etc/pulse/client.conf 
 pip install -r requirements.txt
 mv speakerbot.service /etc/systemd/system/
